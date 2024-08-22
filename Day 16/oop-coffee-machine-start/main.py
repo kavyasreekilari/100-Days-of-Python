00256ac48abebc977acd54d774547768834783e4
kavyakilari <kavyasreekilari@gmail.com>
2024-08-22 11:04:42 -0500

    Day 16 coffee machine using OOP

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -9,19 +9,25 @@ menu = Menu()
 
 is_on = True
 
+# TODO: 1. Print report
+# TODO: 2. Check resources sufficient?
+# TODO: 3. Process coins.
+# TODO: 4. Check transaction succesful?
+# TODO: 5. Make coffee
 
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
+    if choice == "report":
         coffee_maker.report()
         money_machine.report()
+    elif choice == "off":
+        is_on = False
     else:
         drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+        if coffee_maker.is_resource_sufficient(drink) and  money_machine.make_payment(drink.cost):
                 coffee_maker.make_coffee(drink)
+
 
 # turnOff = False
 # def coffee_machine(coffee_selected):
@@ -30,11 +36,7 @@ while is_on:
 #             if CoffeeMaker.is_resource_sufficient(coffee_selected) == True:
 #                 money_entered = MoneyMachine.process_coins()
 #                 if Menu(coffee_selected).menu["cost"]
-#
-#
-#
-#
-#
+
 # while not turnOff:
 #     coffee_selected = input("What would you like? (espresso/latte/cappuccino/): ")
 #     if coffee_selected == "report":
